839e80dc23ae44e92123ec6416c08644eb91728b
Saitama01010 <johnwessam4@gmail.com>
2026-07-26 06:12:15 +0300

    Revert "Build branded dashboard layout studio"
    This reverts commit 6ef9c983f258d18cd45a8d9e5c96d9e3f3c03499.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      data-scroll-behavior="smooth"
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
